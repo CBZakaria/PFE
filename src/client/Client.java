51e26799e0f1dcd533e7e527840ae978f389e718
Your Name <you@example.com>
2018-04-29 20:47:49 +0200

    done

--- src/client/Client.java
+++ src/client/Client.java
@@ -5,12 +5,4 @@ public class Client {
 	public Client() {}
 
 
-
-	public void zak () {System.out.println("cli zak");}
-
-	public void bak () {System.out.println("cli back");}
-	
-	public void zak2() {}
-	
-	public void bak2() {}
 }
